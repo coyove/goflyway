ea593cee386599ee695834865ededcae9d628bf8
coyove <coyove@hotmail.com>
2017-10-13 12:41:51 +0800

    Embed resources into exe

--- shared/goflywin/src/Server.cs
+++ shared/goflywin/src/Server.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -20,34 +21,44 @@ namespace goflywin
                 action();
             }
         }
-
-        public static void PerformSafely<T1>(this Control target, Action<T1> action, T1 parameter)
-        {
-            if (target.InvokeRequired)
-            {
-                target.Invoke(action, parameter);
-            }
-            else
-            {
-                action(parameter);
-            }
-        }
-
-        public static void PerformSafely<T1, T2>(this Control target, Action<T1, T2> action, T1 p1, T2 p2)
-        {
-            if (target.InvokeRequired)
-            {
-                target.Invoke(action, p1, p2);
-            }
-            else
-            {
-                action(p1, p2);
-            }
-        }
     }
 
-    class Util
+    static class Util
     {
+        public static class ResourceManager
+        {
+            private static string _i18n = "zh-CN";
+
+            public static void Use(string i18n)
+            {
+                _i18n = i18n;
+            }
+
+            public static bool GetString(string name, out string value)
+            {
+                string ret;
+                switch (_i18n)
+                {
+                    case "zh-CN":
+                        ret = Form_zh_CN.ResourceManager.GetString(name);
+                        break;
+                    default:
+                        ret = Form_en_US.ResourceManager.GetString(name);
+                        break;
+                }
+
+                value = ret;
+                return ret != null;
+            }
+
+            public static string GetString(string name)
+            {
+                string ret;
+                GetString(name, out ret);
+                return ret;
+            }
+        }
+
         public static string Escape(string text)
         {
             return text.Replace("|", "\\|");
@@ -56,6 +67,84 @@ namespace goflywin
         public static string Unecape(string text)
         {
             return text.Replace("\\|", "|");
+        }
+    }
+
+    static class Config
+    {
+        private static string INI = System.IO.Path.GetDirectoryName(Application.ExecutablePath) + "\\goflywin.ini";
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Unicode)]
+        private static extern long WritePrivateProfileString(string Section, string Key, string Value, string lpFileName);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Unicode)]
+        private static extern int GetPrivateProfileString(string Section, string Key, string Default, StringBuilder RetVal, int Size, string lpFileName);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Ansi)] // note ascii sections only in our .ini
+        private static extern int GetPrivateProfileSectionNames(byte[] lpReturnedString, int nSize, string lpFileName);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Ansi)]
+        private static extern long GetLastError();
+
+        public static List<string> GetSections()
+        {
+            byte[] lpReturnedString = new byte[1 << 16];
+            int len = GetPrivateProfileSectionNames(lpReturnedString, 1 << 16, INI);
+
+            List<string> sections = new List<string>();
+            List<byte> buf = lpReturnedString.Take(len).ToList();
+
+            int ii = 0;
+            for (int i = 0; i < buf.Count; i++)
+            {
+                if (buf[i] == 0)
+                {
+                    if (i == ii) break;
+
+                    sections.Add(Encoding.ASCII.GetString(buf.GetRange(ii, i - ii).ToArray()));
+                    ii = i + 1;
+                }
+            }
+
+            return sections;
+        }
+
+        public static string Read(string section, string key, string defaultValue)
+        {
+            var ret = new StringBuilder(1023);
+
+            if (GetPrivateProfileString(section, key, "", ret, 1023, INI) == 0)
+                return defaultValue;
+
+            return ret.ToString();
+        }
+
+        public static bool ReadBool(string section, string key, bool defaultValue)
+        {
+            string ret = Read(section, key, defaultValue.ToString());
+            return bool.Parse(ret);
+        }
+
+        public static int ReadInt(string section, string key, int defaultValue)
+        {
+            string ret = Read(section, key, defaultValue.ToString());
+            return int.Parse(ret);
+        }
+
+        public static void Write<T>(string section, string Key, T Value)
+        {
+            if (WritePrivateProfileString(section, Key, Value.ToString(), INI) == 0)
+                MessageBox.Show("ini error: " + GetLastError().ToString());
+        }
+
+        public static void DeleteKey(string section, string key)
+        {
+            Write(key, null, section);
+        }
+
+        public static void DeleteSection(string section)
+        {
+            WritePrivateProfileString(section, null, null, INI);
         }
     }
 
@@ -70,37 +159,31 @@ namespace goflywin
         public int UDP;
         public int UDP_TCP;
 
-        public static Server FromString(string text)
+        public static Server FromSection(string section)
         {
-            List<string> parts = new List<string>();
-            int i = 0, ii = 0;
-
-            text += "|";
-            while (i < text.Length)
-            {
-                if (text[i] == '|' && i > 0 && text[i - 1] != '\\')
-                {
-                    parts.Add(Util.Unecape(text.Substring(ii, i - ii)));
-                    i++;
-                    ii = i;
-                    continue;
-                }
-
-                i++;
-            }
-
-            if (parts.Count() < 8) return null;
-
             Server s = new Server();
-            s.ServerAddr = parts[0];
-            s.LocalAddr = parts[1];
-            s.Key = parts[2];
-            s.AuthUser = parts[3];
-            s.AuthPass = parts[4];
-            s.Partial = parts[5] == "1";
-            s.UDP = int.Parse(parts[6]);
-            s.UDP_TCP = int.Parse(parts[7]);
+            s.ServerAddr = Config.Read(section, "Address", "");
+            s.LocalAddr = Config.Read(section, "Local", "");
+            s.Key = Config.Read(section, "Key", "");
+            s.AuthUser = Config.Read(section, "Username", "");
+            s.AuthPass = Config.Read(section, "Password", "");
+            s.Partial = Config.ReadBool(section, "Partial", false);
+            s.UDP = Config.ReadInt(section, "UDP", 8731);
+            s.UDP_TCP = Config.ReadInt(section, "UDPoverTCP", 3);
             return s;
+        }
+
+        public void ToSection()
+        {
+            string section = "server-" + this.ServerAddr;
+            Config.Write(section, "Address", ServerAddr);
+            Config.Write(section, "Local", LocalAddr);
+            Config.Write(section, "Key", Key);
+            Config.Write(section, "Username", AuthUser);
+            Config.Write(section, "Password", AuthPass);
+            Config.Write(section, "Partial", Partial);
+            Config.Write(section, "UDP", UDP);
+            Config.Write(section, "UDPoverTCP", UDP_TCP);
         }
 
         public static Server FromUI(formMain form)
@@ -115,13 +198,6 @@ namespace goflywin
             s.UDP = (int)form.textUDP.Value;
             s.UDP_TCP = (int)form.textUDP_TCP.Value;
             return s;
-        }
-
-        public override string ToString()
-        {
-            return Util.Escape(ServerAddr) + "|" + Util.Escape(LocalAddr) + "|" + Util.Escape(Key) + "|" +
-                Util.Escape(AuthUser) + "|" + Util.Escape(AuthPass) + "|" +
-                (Partial ? "1" : "0") + "|" + UDP.ToString() + "|" + UDP_TCP.ToString();
         }
 
         public void ToUI(formMain form)
