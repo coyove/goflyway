662591ce3c9eebd4d2ba1e8d24f0e5194596b671
coyove <coyove@hotmail.com>
2018-01-06 17:49:34 +0800

    Remove shared (to a branch)

--- shared/build.bat
+++
@@ -1,6 +0,0 @@
-go build -buildmode=c-archive .\main.go
-
-rem gcc .\main.c .\main.a -o main.exe -lWinMM -lntdll -lWS2_32 -Wno-error=address -Wno-pointer-to-int-cast
-rem .\main.exe
-
-gcc .\main.c .\main.a -shared -pthread -o goflyway.dll -lWinMM -lntdll -lWS2_32 -Wno-error=address -Wno-pointer-to-int-cast -Wno-incompatible-pointer-types
