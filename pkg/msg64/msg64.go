d12c95612df08ffbde78f803b0cce3d09850224c
coyove <coyove@hotmail.com>
2018-01-30 08:01:09 +0800

    msg64 refactor to support embedded res

--- pkg/msg64/msg64.go
+++ pkg/msg64/msg64.go
@@ -1,6 +1,9 @@
 package msg64
 
 import (
+	"bytes"
+	"encoding/json"
+	"io"
 	"strings"
 
 	"github.com/coyove/goflyway/pkg/rand"
@@ -17,9 +20,7 @@ func init() {
 	r = rand.New()
 }
 
-func crc16(v interface{}) uint16 {
-	crc := uint16(0)
-
+func crc16(crc uint16, v interface{}) uint16 {
 	switch v.(type) {
 	case []byte:
 		buf := v.([]byte)
@@ -36,19 +37,13 @@ func crc16(v interface{}) uint16 {
 	return crc
 }
 
-type BitsArray struct {
+type bitsArray struct {
 	idx      int
 	bit8     [8]byte
 	underlay []byte
 }
 
-func NewBitsArray(ln int) *BitsArray {
-	return &BitsArray{
-		underlay: make([]byte, 0, ln),
-	}
-}
-
-func (b *BitsArray) push(bit byte) {
+func (b *bitsArray) push(bit byte) {
 	b.bit8[b.idx] = bit
 	b.idx++
 
@@ -63,7 +58,7 @@ func (b *BitsArray) push(bit byte) {
 	}
 }
 
-func (b *BitsArray) Push(bits ...byte) {
+func (b *bitsArray) Push(bits ...byte) {
 	for _, bit := range bits {
 		if bit != 0 && bit != 1 {
 			panic("bit can only be 0 or 1")
@@ -73,7 +68,7 @@ func (b *BitsArray) Push(bits ...byte) {
 	}
 }
 
-func (b *BitsArray) PushByte(n byte, w int) {
+func (b *bitsArray) PushByte(n byte, w int) {
 	if w > 8 || w < 1 {
 		return
 	}
@@ -83,7 +78,15 @@ func (b *BitsArray) PushByte(n byte, w int) {
 	}
 }
 
-func (b *BitsArray) GetBytes() []byte {
+func (b *bitsArray) Write(buf []byte) (int, error) {
+	for _, by := range buf {
+		b.PushByte(by, 8)
+	}
+
+	return len(buf), nil
+}
+
+func (b *bitsArray) GetBytes() []byte {
 	buf := b.underlay
 
 	if b.idx != 0 {
@@ -97,83 +100,86 @@ func (b *BitsArray) GetBytes() []byte {
 	return buf
 }
 
-func (b *BitsArray) RemainingBitsToOneByte() int {
+func (b *bitsArray) RemainingBitsToOneByte() int {
 	return (8 - b.idx) % 8
 }
 
-var table = map[byte]byte{
-	/* 000000 */ 0: 'a',
-	/* 000001 */ 1: 'b',
-	/* 000010 */ 2: 'c',
-	/* 000011 */ 3: 'd',
-	/* 000100 */ 4: 'e',
-	/* 000101 */ 5: 'f',
-	/* 000110 */ 6: 'g',
-	/* 000111 */ 7: 'h',
-	/* 001000 */ 8: 'i',
-	/* 001001 */ 9: 'j',
-	/* 001010 */ 10: 'k',
-	/* 001011 */ 11: 'l',
-	/* 001100 */ 12: 'm',
-	/* 001101 */ 13: 'n',
-	/* 001110 */ 14: 'o',
-	/* 001111 */ 15: 'p',
-	/* 010000 */ 16: 'q',
-	/* 010001 */ 17: 'r',
-	/* 010010 */ 18: 's',
-	/* 010011 */ 19: 't',
-	/* 010100 */ 20: 'u',
-	/* 010101 */ 21: 'v',
-	/* 010110 */ 22: 'w',
-	/* 010111 */ 23: 'x',
-	/* 011000 */ 24: 'y',
-	/* 011001 */ 25: 'z',
-	/* 011010 */ 26: '0',
-	/* 011011 */ 27: '1',
-	/* 011100 */ 28: '2',
-	/* 011101 */ 29: '3',
-	/* 011110 */ 30: '4',
-	/* 011111 */ 31: '5',
-	/* 100000 */ 32: '6',
-	/* 100001 */ 33: '7',
-	/* 100010 */ 34: '8',
-	/* 100011 */ 35: '9',
-	/* 100100 */ 36: 'A',
-	/* 100101 */ 37: 'B',
-	/* 100110 */ 38: 'C',
-	/* 100111 */ 39: 'D',
-	/* 101000 */ 40: 'E',
-	/* 101001 */ 41: 'F',
-	/* 101010 */ 42: '-',
-	/* 101011 */ 43: '.',
-	/* 101100 */ 44: '_',
-	/* 101101 */ 45: 'H',
-	/* 101110 */ 46: ':',
-	/* 101111 */ 47: '/',
-	/* 110000 */ 48: '?',
-	/* 110001 */ 49: '#',
-	/* 110010 */ 50: '+',
-	/* 110011 */ 51: '%',
-	/* 110100 */ 52: ',',
-	/* 110101 */ 53: '!',
-	/* 110110 */ 54: '=',
-	/* 110111 */ 55: '&',
-	/* 111000 */ 56: '*',
-	/* 111001 */ 57: '(',
-	/* 111010 */ 58: ')',
-	/* 111011 */ 59: 'S',
+var table = []byte{
+	/* 000000 0:  */ 'a',
+	/* 000001 1:  */ 'b',
+	/* 000010 2:  */ 'c',
+	/* 000011 3:  */ 'd',
+	/* 000100 4:  */ 'e',
+	/* 000101 5:  */ 'f',
+	/* 000110 6:  */ 'g',
+	/* 000111 7:  */ 'h',
+	/* 001000 8:  */ 'i',
+	/* 001001 9:  */ 'j',
+	/* 001010 10: */ 'k',
+	/* 001011 11: */ 'l',
+	/* 001100 12: */ 'm',
+	/* 001101 13: */ 'n',
+	/* 001110 14: */ 'o',
+	/* 001111 15: */ 'p',
+	/* 010000 16: */ 'q',
+	/* 010001 17: */ 'r',
+	/* 010010 18: */ 's',
+	/* 010011 19: */ 't',
+	/* 010100 20: */ 'u',
+	/* 010101 21: */ 'v',
+	/* 010110 22: */ 'w',
+	/* 010111 23: */ 'x',
+	/* 011000 24: */ 'y',
+	/* 011001 25: */ 'z',
+	/* 011010 26: */ '0',
+	/* 011011 27: */ '1',
+	/* 011100 28: */ '2',
+	/* 011101 29: */ '3',
+	/* 011110 30: */ '4',
+	/* 011111 31: */ '5',
+	/* 100000 32: */ '6',
+	/* 100001 33: */ '7',
+	/* 100010 34: */ '8',
+	/* 100011 35: */ '9',
+	/* 100100 36: */ 'A',
+	/* 100101 37: */ 'B',
+	/* 100110 38: */ 'C',
+	/* 100111 39: */ 'D',
+	/* 101000 40: */ 'E',
+	/* 101001 41: */ 'F',
+	/* 101010 42: */ '-',
+	/* 101011 43: */ '.',
+	/* 101100 44: */ '_',
+	/* 101101 45: */ 'H',
+	/* 101110 46: */ ':',
+	/* 101111 47: */ '/',
+	/* 110000 48: */ 'S',
+	/* 110001 49: */ '#',
+	/* 110010 50: */ '+',
+	/* 110011 51: */ '%',
+	/* 110100 52: */ ',',
+	/* 110101 53: */ '!',
+	/* 110110 54: */ '=',
+	/* 110111 55: */ '&',
+	/* 111000 56: */ '*',
+	/* 111001 57: */ '(',
+	/* 111010 58: */ ')',
 
 	// control bits
-	// 111100    60: HASH, followed by 15 bits
-	// 111101    61: CAPS, followed by 5 bits
-	// 111110    62: RAW, followed by 8 bits, a raw byte
-	// 111111    63: END, end of the message
+
+	// 111011 59: SEP, separator
+	// 111100 60: HASH, followed by 15 bits
+	// 111101 61: CAPS, followed by 5 bits
+	// 111110 62: RAW, followed by 8 bits, a raw byte
+	// 111111 63: END, end of the message
 }
 
 const (
+	_SEP  = 59
 	_HASH = 60
 	_CAPS = 61
 	_RAW  = 62
+	_END  = 63
 )
 
 var itable = map[byte]byte{
@@ -185,8 +191,8 @@ var itable = map[byte]byte{
 	'5': 31, '6': 32, '7': 33, '8': 34, '9': 35,
 	'A': 36, 'B': 37, 'C': 38, 'D': 39, 'E': 40, 'F': 41,
 	'-': 42, '.': 43, '_': 44, 'H': 45, ':': 46, '/': 47,
-	'?': 48, '#': 49, '+': 50, '%': 51, ',': 52, '!': 53,
-	'=': 54, '&': 55, '*': 56, '(': 57, ')': 58, 'S': 59,
+	'S': 48, '#': 49, '+': 50, '%': 51, ',': 52, '!': 53,
+	'=': 54, '&': 55, '*': 56, '(': 57, ')': 58,
 }
 
 func charToIdx(b byte) (byte, byte, int) {
@@ -202,14 +208,19 @@ func charToIdx(b byte) (byte, byte, int) {
 	return b2, 0, 6
 }
 
-func Compress(str string) []byte {
+// Encode encodes a string and a struct into a buffer
+func Encode(str string, payload interface{}) []byte {
 	ln := len(str)
-	if ln == 0 {
-		return []byte{}
-	}
+	b := &bitsArray{underlay: make([]byte, 0, ln*2)}
+	crc := crc16(0, str)
 
-	b := NewBitsArray(ln)
-	crc := crc16(str)
+	var pbuf *bytes.Buffer
+	if payload != nil {
+		pbuf = &bytes.Buffer{}
+		tmp, _ := json.Marshal(payload)
+		json.Compact(pbuf, tmp)
+		crc = crc16(crc, pbuf.Bytes())
+	}
 
 	if strings.HasPrefix(str, "https://") {
 		b.PushByte(3, 2)
@@ -244,64 +255,94 @@ func Compress(str string) []byte {
 		}
 	}
 
+	if payload != nil {
+		b.PushByte(_SEP, 6)
+		b.Write(pbuf.Bytes())
+	}
+
 	w := b.RemainingBitsToOneByte()
 	b.PushByte(0xFF, w)
 
 	return b.GetBytes()
 }
 
-func Decompress(buf []byte) string {
-	idx := 0
-	readidx := 0
+type msgReader struct {
+	buf          []byte
+	idx, readidx int
+	beforeEOF    bool
+	crc          uint16
+}
 
-	_read := func(w int) (byte, bool) {
-		if readidx >= len(buf) {
-			return 0, false
-		}
-
-		curbyte := buf[readidx]
-		// j := 1
-		n := byte(0)
-
-		if idx+w > 8 {
-			// we need next byte
-			if readidx+1 >= len(buf) {
-				return 0xFF, false
-			}
-
-			nextbyte := buf[readidx+1]
-
-			b16 := uint16(curbyte)<<8 + uint16(nextbyte)
-
-			// for i := idx; i < idx+w; i++ {
-			// 	n += byte((b16 >> uint16(15-i) & 0x1) << byte(w-j))
-			// 	j++
-			// }
-			n = byte((b16 >> uint(16-idx-w)) & (1<<uint(w) - 1))
-
-			idx = idx + w - 8
-			readidx++
-
-			return n, true
-		} else {
-			// for i := idx; i < idx+w; i++ {
-			// 	n += (curbyte >> byte(7-i) & 0x1) << byte(w-j)
-			// 	j++
-			// }
-			n = byte((curbyte >> uint(8-idx-w)) & (1<<uint(w) - 1))
-
-			idx += w
-			if idx == 8 {
-				idx = 0
-				readidx++
-			}
-
-			return n, true
-		}
+func (m *msgReader) Read(buf []byte) (int, error) {
+	if m.beforeEOF {
+		return 0, io.EOF
 	}
 
+	for i := 0; i < len(buf); i++ {
+		b, ok := m.read(8)
+		if !ok {
+			m.beforeEOF = true
+			return i, nil
+		}
+
+		buf[i] = b
+		m.crc = ((m.crc << 8) & 0xFF00) ^ crc16Table[((m.crc>>8)&0xFF)^uint16(b)]
+	}
+
+	return len(buf), nil
+}
+
+func (m *msgReader) read(w int) (byte, bool) {
+	if m.readidx >= len(m.buf) {
+		return 0, false
+	}
+
+	curbyte := m.buf[m.readidx]
+	// j := 1
+	n := byte(0)
+
+	if m.idx+w > 8 {
+		// we need next byte
+		if m.readidx+1 >= len(m.buf) {
+			return 0xFF, false
+		}
+
+		nextbyte := m.buf[m.readidx+1]
+
+		b16 := uint16(curbyte)<<8 + uint16(nextbyte)
+
+		// for i := idx; i < idx+w; i++ {
+		// 	n += byte((b16 >> uint16(15-i) & 0x1) << byte(w-j))
+		// 	j++
+		// }
+		n = byte((b16 >> uint(16-m.idx-w)) & (1<<uint(w) - 1))
+
+		m.idx += w - 8
+		m.readidx++
+
+		return n, true
+	}
+	// for i := idx; i < idx+w; i++ {
+	// 	n += (curbyte >> byte(7-i) & 0x1) << byte(w-j)
+	// 	j++
+	// }
+	n = byte((curbyte >> uint(8-m.idx-w)) & (1<<uint(w) - 1))
+
+	m.idx += w
+	if m.idx == 8 {
+		m.idx = 0
+		m.readidx++
+	}
+
+	return n, true
+}
+
+// Decode decodes the buffer into a struct and a string
+func Decode(buf []byte, payload interface{}) string {
+	src := &msgReader{buf: buf}
+
 	ret := make([]byte, 0, len(buf))
-	b, ok := _read(2)
+	b, ok := src.read(2)
 	if !ok {
 		return ""
 	}
@@ -314,37 +355,49 @@ func Decompress(buf []byte) string {
 
 	var crc uint16
 READ:
-	for b, ok := _read(6); ok; b, ok = _read(6) {
+	for b, ok := src.read(6); ok; b, ok = src.read(6) {
 		switch b {
-		case 63:
+		case _END:
 			break READ
 		case _RAW:
-			b2, ok2 := _read(8)
+			b2, ok2 := src.read(8)
 			if !ok2 {
 				break READ
 			}
 
 			ret = append(ret, b2)
 		case _CAPS:
-			b2, ok2 := _read(5)
+			b2, ok2 := src.read(5)
 			if !ok2 {
 				break READ
 			}
 			ret = append(ret, b2+'A')
 		case _HASH:
-			b2, ok2 := _read(7)
-			b3, ok3 := _read(8)
+			b2, ok2 := src.read(7)
+			b3, ok3 := src.read(8)
 			if !ok3 || !ok2 {
 				break READ
 			}
 
 			crc = uint16(b2)<<8 + uint16(b3)
+		case _SEP:
+			src.crc = crc16(0, ret)
+			dec := json.NewDecoder(src)
+			if payload != nil {
+				if dec.Decode(payload) != nil {
+					return ""
+				}
+			}
+			goto VERIFY
 		default:
 			ret = append(ret, table[b])
 		}
 	}
 
-	if (crc16(ret) & 0x7FFF) == crc {
+	src.crc = crc16(0, ret)
+
+VERIFY:
+	if (src.crc & 0x7FFF) == crc {
 		return string(ret)
 	}
 
--- pkg/msg64/msg64_test.go
+++ pkg/msg64/msg64_test.go
@@ -21,13 +21,54 @@ func gen() string {
 	return ret
 }
 
-func TestCompress(t *testing.T) {
+func TestEncodePayload(t *testing.T) {
+	var p struct {
+		A string
+		B int
+	}
+
+	len1, len2 := 0, 0
+
+	rand.Seed(time.Now().UnixNano())
+	test := func(str string) {
+		var p2 struct {
+			A string
+			B int
+		}
+
+		p.A = "www.freeformatter.com"
+		p.B = rand.Intn(65536)
+
+		buf := Encode("", &p)
+
+		t.Error(Base41Encode(buf))
+
+		str2 := Decode(buf, &p2)
+		if str2 != str {
+			t.Errorf("error: \n%s\n%s", str, str2)
+		}
+
+		if p.A != p2.A || p.B != p2.B {
+			t.Errorf("error: \n%v\n%v", p, p2)
+		}
+
+		len1 += len(str)
+		len2 += len(buf)
+	}
+
+	for i := 0; i < 1000; i++ {
+		test(gen())
+	}
+
+	t.Log(float64(len2) / float64(len1))
+}
+
+func TestEncode(t *testing.T) {
 	len1, len2, len3 := 0, 0, 0
 
 	test := func(str string, v bool) {
-		buf := Compress(str)
-
-		str2 := Decompress(buf)
+		buf := Encode(str, nil)
+		str2 := Decode(buf, nil)
 		if str2 != str {
 			t.Errorf("error: \n%s\n%s", str, str2)
 		}
@@ -42,7 +83,7 @@ func TestCompress(t *testing.T) {
 		}
 	}
 
-	for i := 0; i < 100000; i++ {
+	for i := 0; i < 10000; i++ {
 		test(gen(), false)
 	}
 
@@ -53,17 +94,28 @@ func TestCompress(t *testing.T) {
 	test("https://www.facebook.com", true) // 0
 
 	ebuf := []byte{214, 89, 106, 197, 0, 33, 1, 56, 226, 171, 243, 234, 40, 71, 25}
-	t.Log(Decompress(ebuf))
+	t.Log(Decode(ebuf, nil))
 	ebuf[2] = 105
-	t.Log(Decompress(ebuf))
-	t.Log(Decompress([]byte{0}))
+	t.Log(Decode(ebuf, nil))
+	t.Log(Decode([]byte{0}, nil))
 
 	t.Log(float64(len1)/float64(len2), float64(len1)/float64(len3))
 }
 
-func BenchmarkCompress(b *testing.B) {
+func BenchmarkEncode(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		Decompress(Compress(gen()))
+		Decode(Encode(gen(), nil), nil)
+	}
+}
+
+func BenchmarkEncodePayload(b *testing.B) {
+	var p struct {
+		A string
+		B int
+	}
+
+	for i := 0; i < b.N; i++ {
+		Decode(Encode(gen(), &p), &p)
 	}
 }
 
